d374858d2db7341ac680f18b2dd92ceac1b30d28
Александр <velikodnjy@mail.ru>
2023-10-20 22:28:00 +0300

    init

---
+++ Project2.js
@@ -0,0 +1,121 @@
+/*
+var number1 = 5;
+var number2 = 10;
+
+
+var string1 = 'Привет'
+var string2 = 'друг'
+
+console.log(string1 + string2)     
+
+console.log(string1 - string2)     
+
+console.log(string1 / string2)       
+
+console.log(string1 * string2)        
+
+console.log(string1 % string2)        
+
+console.log(number1 + number2)      
+
+console.log(number1 - number2)      
+
+console.log(number1 / number2)      
+
+console.log(number1 * number2)      
+
+console.log(number1 % number2) 
+
+console.log("Test1 =",number1 > number2)          
+
+console.log("Test2 =",number1 < number2)        
+
+console.log("Test3 =",number1 >= number2)      
+
+console.log("Test4 =",number1 <= number2)     
+
+console.log("Test5 =",number1 == number2)      
+
+console.log("Test6 =",number1 != number2)    
+*/
+
+/*
+var myPassword = 'Secret01';
+var testString ='';
+
+if ( myPassword == testString )
+{
+    console.log( "Доступ разрешен" )
+}
+else
+{
+    console.log( "Доступ запрещён" )
+}
+*/
+
+// if(temperature == 0)
+// {
+//     console.log("Холодно");
+// }
+// else if(temperature == 30)
+// {
+//     console.log("Жарко");
+// } else if (temperature == 20){
+//     console.log("Хорошо");
+// }
+// else
+// {
+//     console.log("Хорошая температура");
+// }
+
+
+var temperature = 30;
+
+switch (temperature) {
+case 0:
+    console.log('Cold')
+    break;
+case 20:
+    console.log('Normal')
+    break;
+case 30:
+    console.log('Hot')
+    break;
+default:
+    break;
+}
+
+
+
+/*
+var startNumber = 10;
+
+if(startNumber >= 10)
+{
+    startNumber++;
+}
+if(startNumber != 11)
+{
+    startNumber--;
+}
+if(startNumber == 11)
+{
+    startNumber = startNumber * 2;
+}
+
+console.log(startNumber);
+*/
+
+
+// var firstNumber = 25;
+// var secondNumber = 100;
+
+// if( firstNumber+secondNumber > 100 )
+// {
+//     console.log("Сумма чисел больше ста!");
+// }
+// else
+// {
+//     console.log("Сумма чисел меньше чем сто.");
+// }
+
---
+++ README.md
@@ -0,0 +1,1 @@
+"# CodeFeture_Lessons" 
